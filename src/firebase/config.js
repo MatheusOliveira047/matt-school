3eebde48888e0253e2ea9dad55a9c05ab5431b37
MatheusOliveira047 <ludsonmatheus@gmail.com>
2022-12-22 01:37:30 -0300

    teste com variasveis de ambiente

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey ,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId:process.env.REACT_APP_projectId ,
-  storageBucket:process.env.REACT_APP_storageBucket ,
-  messagingSenderId:process.env.REACT_APP_messagingSenderId ,
-  appId:process.env.REACT_APP_appId,
-  measurementId:process.env.REACT_APP_measurementId
+  apiKey: `${process.env.REACT_APP_apiKey}`,
+  authDomain: `${process.env.REACT_APP_authDomain}`,
+  projectId:`${process.env.REACT_APP_projectId}` ,
+  storageBucket:`${process.env.REACT_APP_storageBucket}` ,
+  messagingSenderId:`${process.env.REACT_APP_messagingSenderId}` ,
+  appId:`${process.env.REACT_APP_appId}`,
+  measurementId:`${process.env.REACT_APP_measurementId}`
 };
 
 const app = initializeApp(firebaseConfig);
